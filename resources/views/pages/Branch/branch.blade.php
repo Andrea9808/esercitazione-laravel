55dcfc3aa3608daf8365550c231c7b6f672437b7
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-05-23 10:29:51 +0200

    feat: view show branches

--- resources/views/pages/Branch/branch.blade.php
+++ resources/views/pages/Branch/branch.blade.php
@@ -4,24 +4,21 @@
 @endsection
 @section('content')
 
-<div class="container vh-100 d-flex align-items-center justify-content-center flex-column">
-    <h1>Tutte le nostre filiali</h1>
+<div class="container h-100 d-flex align-items-center justify-content-center flex-column">
+    <h1 class="mb-5">Tutte le nostre filiali</h1>
     <div class="row">
         @foreach ($branches as $branch)
-            <div class="col-12 col-md-6">
-                <a href="{{route('branch.show', $branch ->id)}}">
+            <div class="col-12 col-md-6 mb-4">
+                <a class="link-route" href="{{route('branch.show', $branch ->id)}}">
                     <div class="card">
                         <h6>Città: {{$branch ->city}}</h6> - <h6>CAP: {{$branch->postalCode}}</h6>
                     </div>
                 </a>
             </div>
         @endforeach
-        <a class="link-home" href="{{route('home')}}">Ritorna alla Home</a>
+        <a class="link-home p-5" href="{{route('home')}}">Ritorna alla Home</a>
     </div>
 </div>
 
 @endsection
 
-<style>
-
-</style>
---
+++ resources/views/pages/Branch/show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main-layout')
+@section('head')
+    <title>{{$branches->city}}</title>
+@endsection
+@section('content')
+
+<div class="container-fluid">
+    {{-- ROTTA RITORNO INDIETRO --}}
+    <a class="d-flex" href="{{route('Branch.branch')}}">
+        <button class="btn btn-info  mx-3"><i class="fa-solid fa-arrow-left"></i></button>
+    </a>
+    <div class="container vh-100 w-100 d-flex align-items-center justify-content-center">
+
+        <div class="card w-50">
+            <h4>Filiale: {{$branches->city}}</h4>
+
+            <div class="row">
+                <div class="col-6">
+                    <h3>Automezzi:</h3>
+                    {{--CICLO GLI AUTOMEZZI PER OGNI FILIALE  --}}
+                    @foreach ($branches->vehicles as $vehicle)
+                        <h6>{{$vehicle->model}}</h6>
+                    @endforeach
+                </div>
+                <div class="col-6">
+                    <h6>Codice filiale: {{$branches->code}}</h6>
+                    <h6>Indirizzo: {{$branches->address}}</h6>
+                    <h6>Città: {{$branches->city}}</h6>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    @endsection
+</div>
+
